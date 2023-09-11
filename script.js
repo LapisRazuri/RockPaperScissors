970055b1f23e48ca834946944b5c0e237eab8e44
LapisRazuri <120453194+LapisRazuri@users.noreply.github.com>
2023-09-11 16:26:51 +0900

    Create button inside the text

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 function getComputerChoice () {
     randomVal = Math.random();
     if (randomVal < 0.3) {
@@ -15,10 +21,14 @@ function getComputerChoice () {
     return randomVal;
     }
 
+
+
+ 
 function singleroundRockPaperScissors () {
+    
     let notAcceptable = true;
     while(notAcceptable){
-        // playerSelection = prompt("What do you choose? Rock? Scissors? or Paper?").toLowerCase();
+        
         if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors"){
             notAcceptable = false;
         }
@@ -29,122 +39,247 @@ function singleroundRockPaperScissors () {
 
     computerSelection = getComputerChoice();
 
-    let roundResult;
+   
     if ((computerSelection == "rock" && playerSelection == "rock") || (computerSelection == "scissors" && playerSelection == "scissors") || (computerSelection == "paper" && playerSelection == "paper")) {
         roundResult = "Draw!";
+        
     }
 
     else if ((computerSelection == "rock" && playerSelection == "scissors") || (computerSelection == "scissors" && playerSelection == "paper") || (computerSelection == "paper" && playerSelection == "rock" )) {
         roundResult = "Computer gets a point!"; 
+        
     }
 
     else if ((computerSelection == "rock" && playerSelection == "paper") || (computerSelection == "scissors" && playerSelection == "rock") || (computerSelection == "paper" && playerSelection == "scissors")) {
         roundResult = "Player gets a point!"; 
+        
 
     }
     
     // console.log("Player picked " + playerSelection + ", Computer picked " + computerSelection +". " + roundResult);
-    const display = document.createElement('div');
-            display.textContent = "Player picked " + playerSelection + ", Computer picked " + computerSelection +". " + roundResult;
-            body.appendChild(display);
-    return roundResult
-}
+    const selection = document.createElement('div');
+    const body = document.querySelector('body');
+    selection.setAttribute('style','font-size:90%; color: black; background-color:white; width:180px; height: 50px; position: absolute; top: 50%')
+    
+    selection.textContent = "Player picked " + playerSelection + ", Computer picked " + computerSelection +". " + roundResult;
+    
+    body.appendChild(selection);
+    
 
-function judge(playerPoints, computerPoints) {
-    if (computerPoints > playerPoints) {
-        return "Computer is the winner!";
-    }
-
-    else if (computerPoints < playerPoints) { 
-        return "Player is the winner!";
-    }
-
-    else {
-        return  "No winner!";
-    }
-}
+    // selection.setAttribute('style', 'font-size:90%; background-color: white; width: 180px; height: 50px');
 
 
-
-function game() {
     let playerPoints = 0;
     let computerPoints = 0;
-
-
-
-
-
-    // for(let i = 0; i < 5; i++) {
-        roundResult = singleroundRockPaperScissors();
-
-        if(roundResult == "Player gets a point!"){
-            playerPoints += 1;
-        }
     
-        else if (roundResult == "Computer gets a point!"){
-            computerPoints += 1;
+        // roundResult = singleroundRockPaperScissors();
+
+            if(roundResult == "Player gets a point!"){
+                playerPoints += 1;
+                }
+        
+            else if (roundResult == "Computer gets a point!"){
+                computerPoints += 1;
+                }
+  
+
+
+    return {playerPoints,
+            computerPoints,
         }
 
-    // }
-        
-    gameState = judge(playerPoints, computerPoints);
-    //const  = document.createElement('div');
-    const displayPlayerPoints = document.createElement('div');
-    displayPlayerPoints.textContent = "Player got " + playerPoints.toString() + " points";
-    body.appendChild(displayPlayerPoints);
-
-    const displayComputerPoints = document.createElement('div');
-    displayComputerPoints.textContent = "Computer got " + computerPoints.toString() + " points";
-    body.appendChild(displayComputerPoints);
-
-    // console.log("Player got " + playerPoints.toString() + " points")
-    // console.log("Computer got " + computerPoints.toString() + " points")
-    console.log(gameState)
 }
 
-//Create div inside the body
-const body = document.querySelector('body');
 
-const div = document.createElement('div');
-body.appendChild(div);
+
+
+// function judge(playerPoints, computerPoints) {
+//     if (computerPoints > playerPoints) {
+//         return "Computer is the winner!";
+//     }
+
+//     else if (computerPoints < playerPoints) { 
+//         return "Player is the winner!";
+//     }
+
+//     else {
+//         return  "No winner!";
+//     }
+// }
+
+
+    
+// function Counts( roundCount ) {
+//     if ( roundCount >= 5 ) {
+//         btn1.style.visibility = "hidden";
+//         btn2.style.visibility = "hidden";
+//         btn3.style.visibility = "hidden";
+//     }
+// }
+
+
+
+
+// const body = document.querySelector('body');
+
+//     const div = document.createElement('div');
+//     body.appendChild(div);
 
 //Create three buttons for each selection 
-for (let i = 0; i < 3; i++ ) {
+//Create button via js requires const buttonname.
+//Need to write function for each of them.
+//Separated to styles in to cssfile, html in to heml file.
 
-    array = ["scissors", "rock", "paper"]
+    // const btn1 = document.createElement('button');
+    // const btn2 = document.createElement('button');
+    // const btn3 = document.createElement('button');
 
-    const btn = document.createElement('button');
+    // btn1.textContent = "Scissors";
+    // btn1.innerHTML = '<img src="images/pexels-karolina-grabowska-4226910.jpg">';
 
+    // btn2.textContent = "Rock";
+    // btn2.innerHTML = '<img src="images/pexels-pixabay-161702.jpg">';
 
-        btn.textContent = array[i];
+    // btn3.textContent = "Paper";
+    // btn3.innerHTML = '<img src="images/pexels-dulce-espinoza-602900.jpg">';
 
-        btn.setAttribute('style', 'color: black; background: coral; width:100px; height:50px; border-color:yellow;' )
-        btn.setAttribute('class', [i])
+ 
+    // btn1.setAttribute('style', 'width:100px; height:50px; border: none; border-radius: 4px;' );
+    // btn2.setAttribute('style', 'width:100px; height:50px; border: none; border-radius: 4px;' );
+    // btn3.setAttribute('style', 'width:100px; height:50px; border: none; border-radius: 4px;' );
 
-        //Associate buttons with the function
+    // div.appendChild(btn1);
+    // div.appendChild(btn2);
+    // div.appendChild(btn3);
 
-        btn.addEventListener('click', () => {
-             playerSelection = array[i];
-             singleroundRockPaperScissors();
-        })
+     // btn2.addEventListener('click', () => {
+    //     playerSelection = "rock";
+    //     singleroundRockPaperScissors()
+    //     })
 
-        body.appendChild(btn);
-    }    
+    // btn3.addEventListener('click', () => {
+    //     playerSelection = "paper";
+    //     singleroundRockPaperScissors();
+    //     })
     
-//Put spaces in-between the boxes
-body.style.cssText = 'display: flex; justify-content:space-around;'
+    
+       
+   
+        // let playerPoints = 0;
+        // let computerPoints = 0;
+    
+        // roundResult = singleroundRockPaperScissors();
+
+        //     if(roundResult == "Player gets a point!"){
+        //         playerPoints += 1;
+        //         }
+        
+        //     else if (roundResult == "Computer gets a point!"){
+        //         computerPoints += 1;
+        //         }
+
+    //     function game() {
+    //     //Below code should appear after five rounds are run
+        
+    //      // console.log(gameState)
+    // } 
 
 
+    //Associate buttons with the function 
+    const button = document.getElementsByClassName('rpsBtn');
+
+    let roundCount = 0; 
+
+    for (let i = 0; i < 3; i++) { 
+        if (button[i]) {
+            button[i].addEventListener('click', () => {
+                if (button[i].innerText || button[i].textContent == "rock") {
+                    playerSelection = "rock";
+                    singleroundRockPaperScissors();
+                    roundCount += 1;
+                    console.log(singleroundRockPaperScissors())
+                }
+    
+                else if (button[i].alt == "scissors") {
+                    playerSelection = "scissors";
+                    singleroundRockPaperScissors();
+                    roundCount += 1;
+                }
+    
+                else if (button[i].alt == "paper") {
+                    playerSelection = "paper";
+                    singleroundRockPaperScissors();
+                    roundCount += 1;
+                }
+            });
+        }
+    }
+
+    function judge(playerPoints, computerPoints) {
+        if (computerPoints > playerPoints) {
+            return "Computer is the winner!";
+        }
+    
+        else if (computerPoints < playerPoints) { 
+            return "Player is the winner!";
+        }
+    
+        else {
+            return  "No winner!";
+        }
+    }
     
 
 
+         if ( roundCount = 5 ) {
+            gameState = judge(playerPoints, computerPoints);
+
+            const totalPlayerPoints = document.getElementsByClassName('totalPlayerPoints');
+            const totalCompPoints = document.getElementsByClassName('totalCompPoints');
+
+        
+            totalPlayerPoints.textContent = "Player got " + playerPoints.toString() + " points";
+            totalCompPoints.textContent = "Computer got " + computerPoints.toString() + " points";
+        
+
+            
+
+            // console.log("Player got " + playerPoints.toString() + " points")
+            // console.log("Computer got " + computerPoints.toString() + " points")
+            const finalWinner = document.getElementsByClassName('judge');
+            finalWinner.textContent = gameState
+            
+            // if (button.innerText || button.textContent == "rock") {
+            //     playerSelection = "rock";
+            //     singleroundRockPaperScissors();
+            //     game();
+            //     roundCount = 0;
+            // }
+
+            // else if (button.innerText == "scissors") {
+            //     playerSelection = "scissors";
+            //     singleroundRockPaperScissors();
+            //     game();
+            //     roundCount = 0;
+            // }
+
+            // else if (button.innerText == "paper") {
+            //     playerSelection = "scissors";
+            //     singleroundRockPaperScissors();
+            //     game();
+            //     roundCount = 0;
+            // }           
+            }
+        
+
+        
+
+        const characterText = document.getElementsByClassName("characterText");
+        const character = document.getElementsByClassName("character");
 
 
+        character[0].addEventListener("click", openText);
 
-
-
-
-
-
-
-
+        function openText() {
+            characterText[0].classList.add("openText");
+       }
